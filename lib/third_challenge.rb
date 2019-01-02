6fa76d2cd4643b270f8bddd63436a4cb46fc677e
Ariel Hicks <hicks.ariel@gmail.com>
2019-01-02 20:55:49 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -4,18 +4,15 @@ def third_challenge
       :patriarch => {
         :name => {"Lord Montague"},
         :age => {"53"},
-        #your key/value pairs here
         },
       :matriarch => {
         :name => {"Lady Montague"},
         :age => {"55"},
-        #your key/value pairs here
         },
       :hero => {
         :name => {"Romeo"},
         :age => {"15"},
         :status => {"alive"},
-        #your key/value pairs here
         },
       :hero_friends => []
    },
@@ -23,18 +20,15 @@ def third_challenge
       :patriarch => {
         :name => {"Lord Capulet"},
         :age => {"50"},
-        #your key/value pairs here
         },
       :matriarch => {
         :name => {"Lady Capulet"},
         :age => {"51"},
-        #your key/value pairs here
         },
       :heroine => {
         :name => {"Juliet"},
         :age => {"15"},
         :status => {"alive"}
-        #your key/value pairs here
         },
       :heroine_friends => []
    }
