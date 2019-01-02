f4e214d902be4308098f470e909dac995b98023a
Ariel Hicks <hicks.ariel@gmail.com>
2019-01-02 21:08:17 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -5,9 +5,12 @@ def fourth_challenge
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
+        :name => {"Benvolio", "Mercutio"},
+        :age => {"17", "18"}
+        :attitude => {"worried", "hot-headed"}
         #your hashes here!
       ]
-   }, 
+   },
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
@@ -18,6 +21,6 @@ def fourth_challenge
    }
 }
 
-  
+
 
 end
