d317c6203410bae62b686300d9883189064256c4
Ariel Hicks <hicks.ariel@gmail.com>
2019-01-02 20:42:34 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -5,7 +5,13 @@ second_challenge = {
                     :matriarch => {},
                     :hero => {},
                     :hero_friends => []
-                  }
+                  },
+          :capulet => {
+            :patriarch => {},
+            :matriarch => {},
+            :heroine => {},
+            :heroine_friends => []
+          }
 
 
 end
